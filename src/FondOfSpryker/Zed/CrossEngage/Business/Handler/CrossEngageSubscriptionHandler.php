14d9f7d75ef39f3010ca242d22652b2279ecadc7
Pascal Fischer <p-fischer@gmx.net>
2020-03-05 09:00:25 +0100

    fix, dont overwrite user/state if already subscribed

--- src/FondOfSpryker/Zed/CrossEngage/Business/Handler/CrossEngageSubscriptionHandler.php
+++ src/FondOfSpryker/Zed/CrossEngage/Business/Handler/CrossEngageSubscriptionHandler.php
@@ -62,7 +62,7 @@ class CrossEngageSubscriptionHandler
             return $this->crossEngageApiClient->createUser($crossEngageTransfer);
         }
 
-        if ($this->storeTransferMapper->getNumericState($crossEngageTransfer) <= 2) {
+        if ($this->storeTransferMapper->getNumericState($fetchedCrossEngageTransfer) <= 2) {
             $fetchedCrossEngageTransfer->fromArray($crossEngageTransfer->toArray(), true);
             $fetchedCrossEngageTransfer = $this->storeTransferMapper->updateEmailState($crossEngageTransfer, CrossEngageConstants::XNG_STATE_NEW);
 
